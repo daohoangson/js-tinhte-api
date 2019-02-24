c8fad223f6ebd798fee4153bd0e106bb0bc114e5
Dao Hoang Son <daohoangson@gmail.com>
2019-02-24 15:21:22 +0700

    Deploy react/demo app on now.sh

--- packages/tinhte-api-react/nwb.config.js
+++ packages/tinhte-api-react/nwb.config.js
@@ -27,20 +27,12 @@ const config = {
     }
   },
   webpack: {
-    html: {
-      filename: 'index.html'
-    },
     extra: {
       plugins: [
         new StatsWebpackPlugin('stats.json', { chunkModules: true })
       ]
     }
   }
-}
-
-if (process.env.NODE_ENV === 'production') {
-  // https://gist.github.com/insin/2b0db9f9fe3922ca57ccda54d8166aba
-  config.webpack.html.filename = '200.html'
 }
 
 module.exports = config
