165004dff95fedab5e3bf4154840b4bc4ce0c7d6
Đào Hoàng Sơn <daohoangson@gmail.com>
2020-05-19 00:22:32 +0700

    Replace `react-tree-walker` dependency with `react-ssr-prepass` (#22)

--- packages/tinhte-api-react/src/helpers/fetchApiDataForProvider.js
+++ packages/tinhte-api-react/src/helpers/fetchApiDataForProvider.js
@@ -1,19 +1,12 @@
-import reactTreeWalker from 'react-tree-walker'
+import ssrPrepass from 'react-ssr-prepass'
 
 const fetchApiDataForProvider = (api, internalApi, rootElement) => {
   const queue = []
 
-  return reactTreeWalker(rootElement, (element) => {
+  return ssrPrepass(rootElement, (element) => {
     if (element && element.type && typeof element.type.apiPreFetch === 'function') {
       element.type.apiPreFetch(api, element, queue)
-
-      // react-tree-walker@4.0.2 haven't added support for React 16.3 Context API
-      // (that means it cannot reach ReactContext.Consumer's function-as-a-child)
-      // we are returning `false ` here to make that dependable
-      return false
     }
-
-    return true
   })
     .then(() => {
       internalApi.log('fetchApiDataForProvider queue.length = %d', queue.length)
--- packages/tinhte-api-react/test/helpers/fetchApiDataForProvider-test.js
+++ packages/tinhte-api-react/test/helpers/fetchApiDataForProvider-test.js
@@ -19,6 +19,37 @@ describe('fetchApiDataForProvider', () => {
       })
   })
 
+  it('returns jobs with a function in apiFetches', () => {
+    const api = apiFactory()
+    const Child = () => 'child'
+    Child.apiFetches = { index: (_, { foo }) => ({ uri: 'index?foo=' + foo }) }
+    const C = api.ConsumerHoc(Child)
+    const P = api.ProviderHoc(() => <C foo='bar' />)
+
+    return api.fetchApiDataForProvider(<P />)
+      .then((apiData) => {
+        const uniqueId = crypt.hashMd5('GET index?foo=bar')
+        expect(Object.keys(apiData)).toContain(uniqueId)
+      })
+  })
+
+  it('returns jobs with nested api consumers', () => {
+    const api = apiFactory()
+    const Grandchild = () => 'grandchild'
+    Grandchild.apiFetches = { grandchild: { uri: 'grandchild' } }
+    const Gc = api.ConsumerHoc(Grandchild)
+    const Child = () => <Gc />
+    Child.apiFetches = { child: { uri: 'child' } }
+    const C = api.ConsumerHoc(Child)
+    const P = api.ProviderHoc(() => <C />)
+
+    return api.fetchApiDataForProvider(<P />)
+      .then((apiData) => {
+        expect(Object.keys(apiData)).toContain(crypt.hashMd5('GET child?'))
+        expect(Object.keys(apiData)).toContain(crypt.hashMd5('GET grandchild?'))
+      })
+  })
+
   it('handles no children', () => {
     const api = apiFactory()
     const P = api.ProviderHoc(() => 'foo')
