2925455427be7000da89ab831e81f4a59405bbb8
Dao Hoang Son <daohoangson@gmail.com>
2021-03-18 09:35:19 +0700

    Remove mime-db mock

--- packages/tinhte-api/web-test-runner.config.mjs
+++ packages/tinhte-api/web-test-runner.config.mjs
@@ -1,13 +1,8 @@
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
-
 import rollupCommonjs from '@rollup/plugin-commonjs'
 import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
-import { importMapsPlugin } from '@web/dev-server-import-maps'
 import { fromRollup } from '@web/dev-server-rollup'
 import { puppeteerLauncher } from '@web/test-runner-puppeteer'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
 const commonjs = fromRollup(rollupCommonjs)
 const nodePolyfills = fromRollup(rollupNodePolyfills)
 
@@ -17,15 +12,6 @@ export default {
   ],
   nodeResolve: true,
   plugins: [
-    importMapsPlugin({
-      inject: {
-        importMap: {
-          imports: {
-            'mime-db': join(__dirname, 'mocks/mime-db.js')
-          }
-        }
-      }
-    }),
     commonjs(),
     nodePolyfills()
   ],
