470180f7e39f59e33dc704e1d7acf73e44aa53cc
Dao Hoang Son <daohoangson@gmail.com>
2021-04-29 14:04:16 +0700

    Rename `CryptoES` -> `Crypto`

--- packages/tinhte-api/src/helpers/crypt.ts
+++ packages/tinhte-api/src/helpers/crypt.ts
@@ -1,16 +1,16 @@
-import CryptoES from 'crypto-js'
+import Crypto from 'crypto-js'
 
 export const algos = ['aes128']
-const aes128EcbPcks7 = { mode: CryptoES.mode.ECB, padding: CryptoES.pad.Pkcs7 }
+const aes128EcbPcks7 = { mode: Crypto.mode.ECB, padding: Crypto.pad.Pkcs7 }
 
 export function encrypt (algo: string, data: string, key: string): string {
   if (algo !== algos[0]) {
     throw new Error('Unsupported algo: ' + algo)
   }
 
-  const keyHashed = CryptoES.MD5(key)
-  const encrypted = CryptoES.AES.encrypt(data, keyHashed, aes128EcbPcks7)
-  const encoded = CryptoES.enc.Base64.stringify(encrypted.ciphertext)
+  const keyHashed = Crypto.MD5(key)
+  const encrypted = Crypto.AES.encrypt(data, keyHashed, aes128EcbPcks7)
+  const encoded = Crypto.enc.Base64.stringify(encrypted.ciphertext)
 
   return encoded
 }
@@ -20,15 +20,15 @@ export function decrypt (algo: string, data: string, key: string): string {
     throw new Error('Unsupported algo: ' + algo)
   }
 
-  const keyHashed = CryptoES.MD5(key)
-  const ciphertext = CryptoES.enc.Base64.parse(data)
-  const cipherParams = CryptoES.lib.CipherParams.create({ ciphertext })
-  const decrypted = CryptoES.AES.decrypt(cipherParams, keyHashed, aes128EcbPcks7)
-  const utf8 = decrypted.toString(CryptoES.enc.Utf8)
+  const keyHashed = Crypto.MD5(key)
+  const ciphertext = Crypto.enc.Base64.parse(data)
+  const cipherParams = Crypto.lib.CipherParams.create({ ciphertext })
+  const decrypted = Crypto.AES.decrypt(cipherParams, keyHashed, aes128EcbPcks7)
+  const utf8 = decrypted.toString(Crypto.enc.Utf8)
 
   return utf8
 }
 
 export function hashMd5 (data: string): string {
-  return CryptoES.MD5(data).toString()
+  return Crypto.MD5(data).toString()
 }
