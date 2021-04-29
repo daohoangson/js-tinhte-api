6620aaa63419da586807e9aa54fa805e2e992ff8
Đào Hoàng Sơn <daohoangson@gmail.com>
2021-04-29 15:15:46 +0700

    Switch back to `crypto-js` (#26)
    * v6.0.0-alpha.2

--- packages/tinhte-api/web-test-runner.config.js
+++ packages/tinhte-api/web-test-runner.config.js
@@ -1,8 +1,10 @@
+const rollupCommonjs = require('@rollup/plugin-commonjs')
 const rollupNodePolyfills = require('rollup-plugin-node-polyfills')
 const rollupTs = require('rollup-plugin-ts')
 const { fromRollup } = require('@web/dev-server-rollup')
 const { puppeteerLauncher } = require('@web/test-runner-puppeteer')
 
+const commonjs = fromRollup(rollupCommonjs)
 const nodePolyfills = fromRollup(rollupNodePolyfills)
 const ts = fromRollup(rollupTs)
 
@@ -15,6 +17,7 @@ module.exports = {
   },
   nodeResolve: true,
   plugins: [
+    commonjs(),
     nodePolyfills(),
     ts()
   ],
