e0db34ba53d69840361c92005728df4f4b95fd33
Dao Hoang Son <daohoangson@gmail.com>
2021-04-29 15:22:26 +0700

    Update Callback.tsx

--- packages/tinhte-api-react/src/components/Callback.tsx
+++ packages/tinhte-api-react/src/components/Callback.tsx
@@ -3,15 +3,15 @@ import { parse } from 'querystring'
 import { ReactApi, ReactApiInternal } from '../types'
 
 export function processCallback (log?: (...args: any[]) => void): boolean {
-  const hash = window.location?.hash?.replace(/^#/, '') ?? ''
+  const hash = window?.location?.hash?.replace(/^#/, '') ?? ''
   const auth = parse(hash)
 
   if (typeof auth.state !== 'string') {
-    log?.call(window, 'Couldn\'t extract state from %s', window.location?.href ?? 'N/A')
+    log?.call(window, 'Couldn\'t extract state from %s', window?.location?.href ?? 'N/A')
     return false
   }
 
-  window.top.postMessage({ auth }, window.location.origin)
+  window?.top?.postMessage({ auth }, window?.location?.origin)
   log?.call(window, 'Forwarded auth to window.top')
 
   return true
